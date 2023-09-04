2328b04139563128f2c689ed71aa0e4f91960d52
Артём Корнилов <temka.kornilov@mail.ru>
2023-09-04 16:56:27 +0300

    Подключение к СУБД

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: "13306",
+  user: 'root',
+  password: 'dev',
+  database: 'node_test'
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+connection.connect();
+
+connection.query('SELECT * FROM `messages`', (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log('Данные из СУБД: ', rows);
+});
+
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 3001;
+app.use(cors());
+
+app.get('/api/messages/all', (req, res) => {
+  connection.query('SELECT * FROM `messages`', (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
